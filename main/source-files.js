07d2e27bcd99804a4226afd3dad24352ad478a93
kl-botsu <botsu@khonsulabs.com>
2021-05-30 02:28:20 +0000

    Deploying to gh-pages from @ khonsulabs/stylecs@70784548340e08b997e64ed58ad4f619e9d12390 🚀

--- main/source-files.js
+++ main/source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["stylecs"] = {"name":"","files":["any.rs","colors.rs","lib.rs","surround.rs"]};
+sourcesIndex["stylecs"] = {"name":"","files":["any.rs","colors.rs","components.rs","lib.rs","style.rs","style_sheet.rs","surround.rs"]};
 sourcesIndex["xtask"] = {"name":"","files":["main.rs"]};
 createSourceSidebar();
