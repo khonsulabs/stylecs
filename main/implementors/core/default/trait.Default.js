0cc210ab951217932b6bd13178c2b464a2c3d00a
kl-botsu <botsu@khonsulabs.com>
2023-05-24 18:28:46 +0000

    Deploying to gh-pages from @ khonsulabs/stylecs@db64e31ba95671e1a2e19ab83dddaefb60145cb0 🚀

--- main/implementors/core/default/trait.Default.js
+++ main/implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"stylecs":[["impl&lt;S: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"stylecs/struct.Style.html\" title=\"struct stylecs::Style\">Style</a>&lt;S&gt;"]]
+"stylecs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"stylecs/struct.Style.html\" title=\"struct stylecs::Style\">Style</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
